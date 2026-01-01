237d914e7e2713e5c44d6d46d4a1df28976de94d
haimengzhao <haimengzhao@icloud.com>
2026-01-01 12:50:18 -0800

    quantum state sketching flat unitary

--- quantum_oracle_sketching.py
+++ quantum_oracle_sketching.py
@@ -4,6 +4,31 @@ from jax import random
 from data_generation import matrix_data, vector_data
 
 
+def q_state_sketch_flat_unitary(data, N):
+    """
+    Construct quantum state sketch preparation unitary from vector data samples.
+    Assume that the vector is flat: all components are plus or minus one.
+
+    Args:
+        data: tuple of (sampled_indices, sampled_values)
+        N: int, dimension of the target quantum state
+
+    Returns:
+        unitary as an array of shape (N, N)
+    """
+    sampled_indices, sampled_values = data
+    num_samples = sampled_indices.shape[0]
+
+    t = jnp.pi * N
+    phase = jnp.zeros(N, dtype=jnp.complex64)
+    phase = phase.at[sampled_indices].add((1 - sampled_values) / 2)
+    phase = phase * t / num_samples
+    diag = jnp.exp(1j * phase)
+    unitary = jnp.diag(diag)
+
+    return unitary
+
+
 def q_state_sketch_flat(data, N):
     """
     Construct quantum state sketch from vector data samples.
@@ -16,21 +41,14 @@ def q_state_sketch_flat(data, N):
     Returns:
         quantum state sketch as an array of shape (N,)
     """
-    sampled_indices, sampled_values = data
-    num_samples = sampled_indices.shape[0]
-
-    t = jnp.pi * N
-    phase = jnp.zeros(N, dtype=jnp.complex64)
-    phase = phase.at[sampled_indices].add((1 - sampled_values) / 2)
-    phase = phase * t / num_samples
-    state = jnp.exp(1j * phase) / jnp.sqrt(N)
-
+    unitary = q_state_sketch_flat_unitary(data, N)
+    # shortcut for diagonal unitary applied to |+> state
+    state = jnp.diag(unitary) / jnp.sqrt(N)
     return state
 
 
-# test q_state_sketch_flat
-
 if __name__ == "__main__":
+    # test q_state_sketch_flat
     print("Testing q_state_sketch_flat...")
 
     # random \pm 1 vector
